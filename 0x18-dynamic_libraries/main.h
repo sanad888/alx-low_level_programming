39d90514dd98c47c73a7611f2b54e2c94abd1c4e
sanad <you@example.com>
2023-06-20 03:56:03 +0300

    fix

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* DYNAMIC_LIB */
+#endif
